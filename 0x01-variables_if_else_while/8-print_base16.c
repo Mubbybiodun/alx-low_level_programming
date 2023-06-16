c209177fe48b02fc4e5f58d7bcfa722a8b7c3172
MubaROCK <mubbybiodun25@gmail.com>
2023-06-16 15:33:12 -0700

    print base 16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -3,16 +3,16 @@
 /**
  * main - Prints numbers btw 0 to 9 and letters btw a to f.
  *followed by a new line
- * Result: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-        int i;
+	int i;
 
-        for(i = 48; i < 58; i++)
-        {
+	for (i = 48; i < 58; i++)
+	{
 		putchar(i);
-        }
-        putchar('\n');
-        return (0);
+	}
+	putchar('\n');
+	return (0);
 }
