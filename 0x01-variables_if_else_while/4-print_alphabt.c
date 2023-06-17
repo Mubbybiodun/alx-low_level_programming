a30bcb03b34d19c07faf14f5e2482416d630cb1c
MubaROCK <mubbybiodun25@gmail.com>
2023-06-17 02:21:59 -0700

    print alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -5,19 +5,19 @@
 /**
  * main - Prints the alphabets without q and e
  *
- * Result: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-        int i;
+	int i;
 
-        for(i = 97; i < 121; i++)
-        {
-		if(i != 101 && i != 113)
+	for (i = 97; i < 121; i++)
+	{
+		if (i != 101 && i != 113)
 		{
-                	putchar(i);
+			putchar(i);
 		}
-        }
-        putchar('\n');
-        return (0);
+	}
+	putchar('\n');
+	return (0);
 }
