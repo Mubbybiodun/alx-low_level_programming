18517f774130fb7b63ad323b05da6b0421df7e82
MubaROCK <mubbybiodun25@gmail.com>
2023-06-17 02:40:43 -0700

    print alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,14 +3,14 @@
 /**
  * main - Prints the alphabets
  *
- * Result: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char alp[52] = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+	char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	int i;
 
-	for(i = 0; i < 52; i++)
+	for (i = 0; i < 52; i++)
 	{
 		putchar(alp[i]);
 	}
