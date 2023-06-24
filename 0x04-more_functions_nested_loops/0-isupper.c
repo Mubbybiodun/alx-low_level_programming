914ca2a5f55522ed2697022c9062d5d20aa5543f
MubaROCK <mubbybiodun25@gmail.com>
2023-06-23 17:34:13 -0700

    update

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - checks for character case
+ *@c: The parameter being passed
+ * Return: 1 for uppercase character and 0 for different character
+ */
+
+int _isupper(int c)
+{
+if (c >= 65 && c <= 90)
+{
+	return (1);
+}
+{
+	return (0);
+}
+}
