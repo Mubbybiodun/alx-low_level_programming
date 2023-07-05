4b82462f2135b4710eeedf737a1fe4ecf393fa1d
MubaROCK <mubbybiodun25@gmail.com>
2023-07-05 16:54:11 -0700

    update

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,10 +7,11 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s)
 	{
-		_putchar('\n');
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
-	_puts_recursion(s + 1);
-	return 0
+	else
+		_putchar('\n');
 }
